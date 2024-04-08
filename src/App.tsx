4d1439e78af667d0cb0ada92366c87779f5422eb
herasymenko-d <dmherasymenko@gmail.com>
2024-04-08 13:58:25 +0300

    add logIn request

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import { supabase } from "./lib/helper/supabaseClient";
 
-function App() {
-  const [count, setCount] = useState(0)
+export const App = () => {
+  const [user, setUser] = useState();
+
+  useEffect(() => {
+
+   const { data: { subscription } } =  supabase.auth.onAuthStateChange((event, session) => {
+      switch(event) {
+        case 'SIGNED_IN':
+          setUser(session?.user);
+          break;
+        case 'SIGNED_OUT':
+          setUser(null);
+          break;
+      }
+    })
+    return () => subscription.unsubscribe()
+  }, []);
+
+  const handleLogin = async () => {
+       await supabase.auth.signInWithOAuth({
+        provider: "github",
+      });
+
+  };
+
+
+  const handleLogOut = async () => { 
+    await supabase.auth.signOut();
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {user ? (
 
-export default App
+        <div>
+        <h1>Authificated</h1>
+        <button onClick={handleLogOut}>LogOut</button>
+        </div>
+
+      ) : (
+        <button onClick={handleLogin}>Login with Github</button>
+      )}
+    </div>
+  );
+};
--- src/lib/helper/supabaseClient.ts
+++ src/lib/helper/supabaseClient.ts
@@ -1,4 +1,5 @@
 import { createClient } from '@supabase/supabase-js'
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient(import.meta.env.REACT_APP_SUPABASE_URL, import.meta.env.REACT_APP_SUPABASE_ANON_KEY)
+
+export const supabase = createClient(process.env.REACT_APP_SUPABASE_URL, process.env.REACT_APP_SUPABASE_ANON_KEY)
+
